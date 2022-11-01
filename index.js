ed687b2d70743b9ada8b9c8a23d65a57956b7473
Tomm223 <dan.osipov99@mail.ru>
2022-11-01 15:49:28 +0300

    check cors

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 app.use(cors({
-    credentials: true,
-    origin: "*"
+  credentials: true,
+  origin: "http://localhost:3000",
 }));
 app.use(cookieParser());
 app.use(express.json())
@@ -30,31 +30,31 @@ app.use("/api", apiRouter)
 
 //socket logic
 async function updateServerData() {
-    const { list, last_update } = await socketService.updateData()
-    GlobalMarketData.update(list, last_update) // кладем данные
-    io.emit('update_data')
+  const { list, last_update } = await socketService.updateData()
+  GlobalMarketData.update(list, last_update) // кладем данные
+  io.emit('update_data')
 }
 
 const start = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://daniil:daniil@cluster0.06smrvn.mongodb.net/?retryWrites=true&w=majority`)
-        server.listen(PORT, async () => {
-            console.log(`server started on port ${PORT}`)
-            // забираем из монго данные для пользования
-            const { last_update, list } = await Market.findOne({ name: 'global' })
-            GlobalMarketData.update(list, last_update) // кладем данные
-            //проверка на валидност данных
-            //если с монго придут данные старее чем время сейчас больше чем на 5 мин то updateserver()
-            const isValid = socketService.validateLastUpdate(getUnix(), last_update)
-            if (!isValid) {
-                updateServerData()
-            }
-            //обновление сервер данных
-            setTimeout(updateServerData, socketService.time_interval_ms)
-        })
-    } catch (e) {
-        console.log(e)
-    }
+  try {
+    await mongoose.connect(`mongodb+srv://daniil:daniil@cluster0.06smrvn.mongodb.net/?retryWrites=true&w=majority`)
+    server.listen(PORT, async () => {
+      console.log(`server started on port ${PORT}`)
+      // забираем из монго данные для пользования
+      const { last_update, list } = await Market.findOne({ name: 'global' })
+      GlobalMarketData.update(list, last_update) // кладем данные
+      //проверка на валидност данных
+      //если с монго придут данные старее чем время сейчас больше чем на 5 мин то updateserver()
+      const isValid = socketService.validateLastUpdate(getUnix(), last_update)
+      if (!isValid) {
+        updateServerData()
+      }
+      //обновление сервер данных
+      setTimeout(updateServerData, socketService.time_interval_ms)
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 start()
@@ -63,3 +63,64 @@ start()
 
 
 
+/*
+ Построение на стеке Heroku-22
+-----> Определение того, какой пакет сборки использовать для этого приложения
+-----> Обнаружено приложение Node.js
+       
+-----> Создание среды выполнения
+       
+       NPM_CONFIG_LOGLEVEL=ошибка
+       NODE_VERBOSE=ложь
+       NODE_ENV=производство
+       NODE_MODULES_CACHE=истина
+       
+-----> Установка бинарников
+       engine.node (package.json): не указано
+       Engines.npm (package.json): не указано (используйте по умолчанию)
+       
+       Разрешение узла версии 18.x...
+       Загрузка и установка узла 18.12.0...
+       Использование версии npm по умолчанию: 8.19.2
+       
+-----> Установка зависимостей
+       Установка узловых модулей
+       
+       added 260 packages, and audited 261 packages in 4s
+       
+       20 packages are looking for funding
+         run `npm fund` for details
+       
+       14 vulnerabilities (7 moderate, 4 high, 3 critical)
+       
+       To address all issues, run:
+         npm audit fix
+       
+       Run `npm audit` for details.
+       
+-----> Build
+       
+-----> Caching build
+       - npm cache
+       
+-----> Pruning devDependencies
+       
+       up to date, audited 261 packages in 2s
+       
+       20 packages are looking for funding
+         run `npm fund` for details
+       
+       14 vulnerabilities (7 moderate, 4 high, 3 critical)
+       
+       To address all issues, run:
+         npm audit fix
+       
+       Run `npm audit` for details.
+       
+-----> Build succeeded!
+-----> Discovering process types
+       Procfile declares types     -> (none)
+       Default types for buildpack -> web
+-----> Compressing...
+       Done: 45.3M
+*/
